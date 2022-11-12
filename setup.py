304f620dfbc62c5982b6879ac63311e4ab9df984
ubalklen <42127323+ubalklen@users.noreply.github.com>
2022-11-12 09:29:14 -0300

    fixed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="untable",
-    version=0.5,
+    version=0.1,
     long_description=long_description,
     long_description_content_type="text/markdown",
     project_urls={
